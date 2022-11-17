9b4fc54f2cd534afcdba0e44789a08a2e3b3cb08
heldercambuta <heldercambuta44@gmail.com>
2022-11-17 13:52:36 +0100

    Actualização

--- codes/app-frontend/src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-<template>
-  <div class="bg-slate-500 w-96 h-96 flex justify-center items-center">
-    <h1>TailwinCSS is Working</h1>
-  </div>
-</template>
--- codes/app-frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import './css/index.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
