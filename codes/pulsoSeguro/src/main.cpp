ec54f286a697aab7798f5fd840aeaec7f7a41806
ClaudioCanga <115471107+ClaudioCanga@users.noreply.github.com>
2023-06-06 13:39:24 +0100

    up

--- codes/pulsoSeguro/src/main.cpp
+++ codes/pulsoSeguro/src/main.cpp
@@ -2,11 +2,11 @@
 #include <SPI.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 #include "MAX30105.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_MLX90614.h>
 
 // Substitua com as suas credenciais do Adafruit IO
 #define WIFI_SSID "Pulso Seguro"
@@ -27,18 +27,19 @@ Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_CLIENT_ID, MQTT_
 
 // Crie o objeto Adafruit_MQTT_Publish para publicar dados no Adafruit IO
 Adafruit_MQTT_Publish bpm = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/bpm1");
+Adafruit_MQTT_Publish temObjecto = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temObjecto1");
+Adafruit_MQTT_Publish temAmbiental = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temAmbiental1");
 
-// Crie um objeto MAX30105_Pulse para ler os dados do sensor MAX30102
+Adafruit_MLX90614 mlx;
 MAX30105 particleSensor;
-
-// Define as configurações do display OLED
-#define OLED_RESET D0
-Adafruit_SSD1306 display(OLED_RESET);
 
 void setup() {
   Serial.begin(9600);
-  delay(1000);
-  
+  Wire.begin();
+
+  mlx.begin(0x5A); // Define o endereço I2C do sensor MLX90614 como 0x5A
+  particleSensor.begin(Wire, I2C_SPEED_FAST, 0x57); // Define o endereço I2C do sensor MAX30102 como 0x57
+
   // Conecte-se à rede Wi-Fi
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.print("Conectando à rede Wi-Fi");
@@ -49,25 +50,6 @@ void setup() {
   Serial.println("");
   Serial.println("Conectado à rede Wi-Fi");
 
-  // Inicialize o display OLED
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.display();
-  delay(2000);
-  display.clearDisplay();
-
-  // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-  {
-    Serial.println("MAX30102 was not found. Please check wiring/power. ");
-    while (1);
-  }
-  Serial.println("Place your index finger on the sensor with steady pressure.");
-
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
-
-
   // Conecte-se ao servidor MQTT
   Serial.print("Conectando ao servidor MQTT...");
   while (!mqtt.connected()) {
@@ -77,36 +59,49 @@ void setup() {
     }
   }
   Serial.println("Conectado ao servidor MQTT");
+  particleSensor.setup(); //Configure sensor with default settings
+  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
-  // Inscreva-se no feed "bpm" do Adafruit IO
+
+  // Inscreva-se nos feeds do Adafruit IO
   Adafruit_MQTT_Subscribe bpm_sub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/bpm1");
+  Adafruit_MQTT_Subscribe temObjecto_sub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/temObjecto1");
+  Adafruit_MQTT_Subscribe temAmbiental_sub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/temAmbiental1");
   mqtt.subscribe(&bpm_sub);
+  mqtt.subscribe(&temObjecto_sub);
+  mqtt.subscribe(&temAmbiental_sub);
 }
 
 void loop() {
+  // Leia a temperatura do sensor MLX90614
+  float ambientTemp = mlx.readAmbientTempC();
+  float objectTemp = mlx.readObjectTempC();
+
   // Leia a frequência cardíaca do sensor MAX30102
   uint32_t irValue = particleSensor.getIR();
   irValue = irValue / 1000;
 
-  // Atualize o display OLED
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println("Frequencia Cardiaca");
-  display.setTextSize(3);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(20, 30);
-  display.println(irValue);
-  display.display();
-  
-  // Publique o valor no feed "bpm" do Adafruit IO
+  // Publique os valores nos feeds correspondentes do Adafruit IO
   Serial.print("Publicando no feed 'bpm': ");
   Serial.println(irValue);
   if (!bpm.publish(irValue)) {
     Serial.println("falha ao publicar");
   }
-  
-  // Aguarde 10 segundos antes de ler novamente
-  delay(3000);
+
+  Serial.print("Publicando no feed 'temObjecto': ");
+  Serial.print(objectTemp);
+  Serial.print(" °C");
+  if (!temObjecto.publish(objectTemp)) {
+    Serial.println("falha ao publicar");
+  }
+
+  /*Serial.print("Publicando no feed 'temAmbiental': ");
+  Serial.print(ambientTemp);
+  Serial.print(" °C");
+  if (!temAmbiental.publish(ambientTemp)) {
+    Serial.println("falha ao publicar");
+  }*/
+
+  delay(1000); // Aguarde 1 segundo antes de ler novamente
 }
